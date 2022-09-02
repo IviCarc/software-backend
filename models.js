d0f7057561c6af17c447e0b0476b5fe7b48f8048
ivan-notebook <kovachivanagustin@gmail.com>
2022-09-01 21:10:00 -0300

    cambios schema

--- models.js
+++ models.js
@@ -1,21 +1,44 @@
 const mongoose = require("mongoose");
 const {Schema} = mongoose;
 
-const ProductosSchema = new Schema({
-    producto : String,
-    precio : Number,
-    descripcion : String,
-    categoria : String,
-    cantidadDisponible : Number,
-    imagenes : Buffer
+const VendedorSchema = new Schema ({
+    nombre:String,
+    apellido:String,
+    email:String,
+    contraseña:String, // FALTA HASH
+    direccion:String,
+    paisResidencia:String,
+    telefono: Number,
+    // HISTORIAL
+    // publicaciones : [ProductosSchema]
 })
 
-const CategoriaSchema  = new Schema ({
-    categoria : String,
+console.log(VendedorSchema.telefono) // BUSCAR POPULATION
+
+const ProductosSchema = new Schema({
+    nombre : String,
+    descripcion : String,
+    caracteristicas : String,
+    stock : Number,
+    precio : Number,
+    imagenes : Buffer,
+    vendedor : VendedorSchema,
+})
+
+const SubcategoriaSchema = new Schema ({
+    nombre: String,
     productos : [ProductosSchema]
 })
 
+const CategoriaSchema  = new Schema ({
+    nombre : String,
+    subcategorias : [SubcategoriaSchema]
+})
+
+
+
 const Producto = mongoose.model("Producto", ProductosSchema);
+const Subcategoria = mongoose.model("Subcategoria", SubcategoriaSchema);
 const Categoria = mongoose.model("Categoria", CategoriaSchema);
 
-module.exports = {Categoria, Producto}
+module.exports = {Categoria, Producto, Subcategoria}
